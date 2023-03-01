e640984c75294101ad5da9835818c7fba1a36a1a
Noah <noahhrbth@icloud.de>
2023-03-01 22:11:41 +0100

    update openai package; replace old model with gpt-3.5-turbo model; add that the bot remembers prev conversations

--- src/bot.js
+++ src/bot.js
@@ -14,23 +14,41 @@ const { Client, Discord } = require("discord.js");
 const client = new Client();
 
 const PREFIX = "/";
+const messages = [];
 client.on("message", async (msg) => {
 	if (!msg.content || msg.author.bot || !msg.content.startsWith(PREFIX)) return;
 
 	try {
+		// Get user input
 		const prompt = msg.content.replace(PREFIX, "");
-
-		// Send request to OpenAI API
-		const completion = await openai.createCompletion({
-			model: "text-davinci-003",
-			prompt: prompt,
-			temperature: 0.2,
-			max_tokens: 100,
+		// Add user input to messages array
+		messages.push({
+			role: "user",
+			content: prompt,
 		});
 
-		// Send response to Discord
-		const response = completion.data.choices[0].text;
-		msg.reply(response);
+		// Send request to OpenAI API
+		// new model: gpt-3.5-turbo
+		const completion = await openai.createChatCompletion({
+			model: "gpt-3.5-turbo",
+			messages: [...messages],
+		});
+
+		// old model: text-davinci-003
+		// const completion = await openai.createCompletion({
+		// 	model: "text-davinci-003",
+		// 	prompt: prompt,
+		// 	temperature: 0.2,
+		// 	max_tokens: 100,
+		// });
+
+		const response = completion.data.choices[0].message;
+
+		// Add bot response to messages array
+		messages.push(response);
+
+		// Send bot response to Discord
+		msg.reply(response.content);
 	} catch (error) {
 		// Consider implementing your own error handling logic here
 		console.error(error);
